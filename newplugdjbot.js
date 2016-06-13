7a5d41b80b5ad2c279a6a535aa123832430df22c
Rewindz <hx3er5now@gmail.com>
2016-06-12 23:23:54 -0400

    V1.1
    Adding things slowly

--- newplugdjbot.js
+++ newplugdjbot.js
@@ -1,10 +1,44 @@
 //------  PLUG.DJ BOT 2016+ -----//
 //------ BY STEAM ID DEFENDERREWINDZ -----//
+//-- To Do:
+//-- Menu
+//-- Black List/White List
+//-- 
 
 prevSong = "";
+doEcho = true;
+doWoot = true;
 
-function startBot(){
+function createMenu(){
+	list = $('.list')[2];
+	
+	wootButton = document.createElement('div');
+	wootButton.class = 'item';
+	wootButton.onclick = 'wootClick';
+	wootButton.innerHTML = '<span>Toggle Auto Woot</span>';
+	
+	echoButton = document.createElement('div');
+	echoButton.class = 'item';
+	echoButton.onclick = 'echoClick';
+	echoButton.innerHTML = '<span>Toggle Echoing</span>';
+	
+	list.appendChild(wootButton);
+	list.appendChild(echoButton);
+}
+
+function wootClick(){
+	doWoot = !doWoot;
+}
+
+function echoClick(){
+	doEcho = !doEcho;
+}
+
+function wootCheck(){
 	echo("Woot bot checking...");
+	if(!doWoot){
+		echo("Auto-Woot disabled!");
+	}
 	if(API.getMedia().cid != prevSong){
 		echo("Song has changed wooting!");
 		woot();
@@ -15,6 +49,7 @@ function startBot(){
 }
 
 function echo(msg){
+	if(!doEcho)return;
 	console.log("REWINDZ PLUGDJ BOT: " + msg);
 }
 
@@ -22,5 +57,6 @@ function woot(){
 	$('#woot').click();
 }
 
-startBot();
-window.setInterval(startBot, 5000);
+createMenu();
+
+window.setInterval(wootCheck, 5000);
